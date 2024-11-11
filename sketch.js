f738be0f8d12c36ba418a6ce20c7d7e55cd86979
pato <114608787+duckgren@users.noreply.github.com>
2024-11-11 10:16:41 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,55 @@
+let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
+
+function draw() {
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "shrek";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "velozes e furiosos";          
+        } else{
+         return "tinker bell";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "narnia";
+        } else {
+          return "piratas do caribe";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "guardiões da galaxia";
+    } else {
+      return "o menino que descobriu o vento";
+    }
+  }
+}
+
 
